b25106d7981f743fd9b4995ca6b61eba5c900876
randall-romero <romero-aguilar.1@osu.edu>
2015-03-20 01:42:06 -0400

    dem01_basis updated to make use of the __call__ method for BasisChebyshev

--- demos/dem01_basis.py
+++ demos/dem01_basis.py
@@ -8,7 +8,7 @@ __author__ = 'Randall'
     Licensed under the MIT license, see LICENSE.txt
 """
 
-import basisChebyshev as bc
+from pycompecon.basisChebyshev import BasisChebyshev
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -19,27 +19,26 @@ import matplotlib.pyplot as plt
 """
 
 # First, create the function f
-def f(x):
-    return np.mat(1 + np.sin(2 * x)).T
+f = lambda x: np.mat(1 + np.sin(2 * x)).T
 
 # and the Chebyshev basis B. If the type of nodes is unspecified, Gaussian is computed by default
-B = bc.BasisChebyshev(5,0,np.pi)
+Phi = BasisChebyshev(5,0,np.pi)
 
 # Interpolation matrix and nodes: Obtain the interpolation matrix Phi, evaluated at the basis nodes.
-Phi = B.interpolation()
+
 
 # The basis nodes are:
-xnodes = B.nodes
+xnodes = Phi.nodes
 
 # Fitting a function: set the interpolation coefficients c:
-c = np.mat(np.linalg.solve(Phi[0], f(xnodes)))  # notice that Phi is a dictionary, the matrix is indexed by 0
+c = np.mat(np.linalg.solve(Phi(), f(xnodes)))  # notice that Phi is a dictionary, the matrix is indexed by 0
 
 # Next plot the function f and its approximation. To evaluate the function defined by the basis B and coefficients c
 # at values xx we use the interpolation method:
 
 # We also plot the residuals, showing the residuals at the interpolating nodes (zero by construction)
-xx = np.linspace(B.a, B.b, 121)
-Phi_x = B.interpolation(xx)[0]
+xx = np.linspace(Phi.a, Phi.b, 121)
+Phi_x = Phi(xx)
 f_approx = Phi_x * c
 fxx = f(xx)
 
@@ -55,15 +54,30 @@ plt.plot(xx.T, f_approx - fxx)
 plt.title('Residuals using 5 nodes')
 
 # Adjusting the number of nodes: to increase accuracy, we increase the number of nodes in B to 25.
-B.n = 25
-c2 = np.mat(np.linalg.solve(B.interpolation(B.nodes)[0], f(B.nodes)))
+Phi.n = 25
+c2 = np.mat(np.linalg.solve(Phi(), f(Phi.nodes)))
 plt.figure()
 plt.subplot(2,1,1)
 plt.plot(xx.T, f(xx))
-plt.plot(xx.T, B.interpolation(xx)[0] * c2)
+plt.plot(xx.T, Phi(xx) * c2)
 plt.legend(['f = 1 + sin(2x)','approx.'])
 plt.title('Chebyshev approximation with 25 nodes')
 
 plt.subplot(2,1,2)
-plt.plot(xx.T, B.interpolation(xx)[0] * c2 - f(xx),'r')
+plt.plot(xx.T, Phi(xx) * c2 - f(xx),'r')
 plt.title('Residuals using 25 nodes')
+
+# With previous basis, now compute derivative.
+df = lambda x: np.mat(2 * np.cos(2 * x)).T
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.plot(xx.T, df(xx))
+plt.plot(xx.T, Phi(xx, 1) * c2)  # notice the 1 in Phi(xx,1) to compute first derivative
+plt.legend(['df/dx = 2cos(2x)', 'approx.'])
+plt.title('Chebyshev approximation with 25 nodes')
+
+plt.subplot(2, 1, 2)
+plt.plot(xx.T, Phi(xx, 1) * c2 - df(xx), 'r')
+plt.title('Residuals using 25 nodes')
+
+plt.show()
