a8c62ac8c1a97475345e481e184ccc9aed8f7ed9
randall-romero <romero-aguilar.1@osu.edu>
2015-03-23 03:15:39 -0400

    instances of Interpolator created from a single Basis share their parent Basis attributes (memory), only data on y and c is instance-specific  to manipulate several functions in a singe object, just create an array of Interpolators

--- compecon/interpolator.py
+++ compecon/interpolator.py
@@ -66,49 +66,56 @@ from scipy import linalg
 
 class Interpolator(Basis):
     def __init__(self, *args, y=None, **kwargs):
+
         if type(args[0]) == Basis:
             B = args[0]
-            Basis.__init__(self, B.n, B.a, B.b, **B.opts)
         else:
-            Basis.__init__(self,*args, **kwargs)
+            B = Basis()
+            Basis.__init__(B, *args, **kwargs)
 
         # Compute interpolation matrix at nodes
-        _PhiT = self.interpolation().T
-        self._PhiT = _PhiT
+        _PhiT = B.interpolation().T
+        B._PhiT = _PhiT
 
         # Compute inverse if not spline
-        if self.type == 'chebyshev':
-            self._PhiInvT = linalg.solve(np.dot(_PhiT, _PhiT.T), _PhiT).T
+        if B.type == 'chebyshev':
+            B._PhiInvT = linalg.solve(np.dot(_PhiT, _PhiT.T), _PhiT).T
         else:
             raise NotImplementedError
 
+
+        # share data in this basis with all instances
+        self.__dict__ = copy.copy(B.__dict__)
+
+
+
         # add data
-        self._y = np.zeros([self.N]) if y is None else y
+        if y is None:
+            y = np.zeros([self.N])
+        elif isinstance(y, (list, np.ndarray)):
+            y = np.asarray(y)
+            y = y.reshape([y.size])
+        else:
+            raise ValueError('y must be a list or numpy array with {} elements'.format(self.N))
+
+        if y.size != self.N:
+            raise ValueError('y must be a list or numpy array with {} elements'.format(self.N))
+
+        self._y = y
         self._c = np.dot(self._y, self._PhiInvT)
-        self._oldy = copy.copy(self._y)
-        self._oldc = copy.copy(self._c)
+        self._yIsOutdated = False
+        self._cIsOutdated = False
 
 
 
     """ setter and getter methods """
 
     @property
-    def _yIsOutdated(self):
-        """ :return: True iff _c and _oldc have same shape and values """
-        return not np.array_equal(self._c, self._oldc)
-
-    @property
-    def _cIsOutdated(self):
-        """ :return: True iff _y and _oldy have same shape and values """
-        return not np.array_equal(self._y, self._oldy)
-
-    @property
     def y(self):
         """ :return: function values at nodes """
         if self._yIsOutdated:
             self._y = np.dot(self._c, self._PhiT)
-            self._oldy = copy.copy(self._y)
-            self._oldc = copy.copy(self._c)
+            self._yIsOutdated = False
         return self._y
 
     @property
@@ -116,8 +123,7 @@ class Interpolator(Basis):
         """ :return: interpolation coefficients """
         if self._cIsOutdated:
             self._c = np.dot(self._y, self._PhiInvT)
-            self._oldc = copy.copy(self._c)
-            self._oldy = copy.copy(self._y)
+            self._cIsOutdated = False
         return self._c
 
     @property
@@ -128,25 +134,30 @@ class Interpolator(Basis):
 
     @y.setter
     def y(self, val):
-        print('calling the y setter')
-        if val.ndim == 1:
-            assert(val.size == self.N)
-        elif val.shape[-1] != self.N:
-            raise ValueError('last index of value must have {} values (one for each node)'.format(self.N))
+        if isinstance(val, (list, np.ndarray)):
+            val = np.asarray(val)
+            val = val.reshape([val.size])
+        else:
+            raise ValueError('y must be a list or numpy array with {} elements'.format(self.N))
+
+        if val.size != self.N:
+            raise ValueError('val must be a list or numpy array with {} elements'.format(self.N))
         self._y = val
+        self._yIsOutdated = False
+        self._cIsOutdated = True
 
     @c.setter
     def c(self, val):
-        print('calling the c setter')
-        assert (val.shape[-1] == self.M)  # one value per polynomial
+        assert (val.size == self.M)  # one value per polynomial
         self._c = val
-
+        self._cIsOutdated = False
+        self._yIsOutdated = True
 
 
 
     """  Interpolation method """
 
-    def __call__(self,x = None, order = None):
+    def __call__(self,x=None, order=None):
         """
 
         :param x:
@@ -167,7 +178,9 @@ class Interpolator(Basis):
         else:
             return np.array([np.dot(self.c, Phix[k].T) for k in range(order.shape[1])])
 
-
+"""
+        NO LONGER NEEDED: INTERPOLATOR ONLY HANDLES ONE FUNCTION AT A TIME:
+        For more dimension, just make a numpy.array from a list comprehension
 
     def __getitem__(self, item):
         Fij = copy.copy(self)
@@ -186,3 +199,5 @@ class Interpolator(Basis):
 
 
 # todo: need to fix the setter methods: they do not adjust the other value when set with a subset
+
+"""
