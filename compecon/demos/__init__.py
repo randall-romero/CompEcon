d2d23cda3b78c82903933979b93c55ba521355ab
randall-romero <romero-aguilar.1@osu.edu>
2017-06-11 23:38:59 -0600

    set figures parameters in demo init

--- compecon/demos/__init__.py
+++ compecon/demos/__init__.py
@@ -7,6 +7,31 @@ The demos folder contains (sometimes different) versions of the original demos.
 """
 
 from .setup import demo
-import pkgutil
-pkgutil.extend_path(__path__, __name__)
+
+import numpy as np
+import warnings
+import seaborn as sns
+sns.set_style('dark')
+np.set_printoptions(4, linewidth=120)
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from warnings import simplefilter
+simplefilter('ignore')
+
+
+from matplotlib import rcParams
+rcParams['lines.linewidth'] = 2.5
+rcParams['figure.subplot.hspace'] = 0.25
+rcParams['legend.frameon'] = False
+rcParams['savefig.directory'] = './figures/'
+rcParams['savefig.format'] = 'pdf'
+rcParams['font.size'] = 24
+
+mpl.rc('lines', linewidth=2.5)
+mpl.rc('font', size=18)
+mpl.rc('savefig', directory='./figures', format='pdf')
+plt.rcParams['figure.figsize'] = 12, 6
+
+from compecon import tic, toc
 
