fixed bug in hessian

compecon/optimize.py
@@ -128,29 +128,27 @@ class OP(Options_Container):
         # Update solution options using kwargs
         self.opts[kwargs.keys()] = kwargs.values()
         self.opts.method = 'golden'
-        alpha1 = (3 - np.sqrt(5)) / 2
-        alpha2 = (np.sqrt(5) - 1) / 2
+        φ = (np.sqrt(5) - 1) / 2  # golden ratio
 
         if a > b:
             a, b = b, a
 
-        d = b - a
-        x1 = a + alpha1 * d
-        x2 = a + alpha2 * d
+        Δ = (b - a) * (1 - φ)
+        x = a + Δ
+        y = b - Δ
+        fx, fy = self.f(x), self.f(y)
 
-        f1, f2 = self.f(x1), self.f(x2)
+        Δ *= φ
+        while Δ > self.opts.tol:
+            Δ *= φ
+            if fy < fx: # y is new upper bound
+                x, y = x - Δ, x
+                fx, fy = self.f(x), fx
+            else:  # x is new lower bound
+                x, y = y, y + Δ
+                fx, fy = fy, self.f(y)
 
-        d *= (alpha1 * alpha2)
-        while d > self.opts.tol:
-            d *= alpha2
-            if f2 < f1: # x2 is new upper bound
-                x2, x1 = x1, x1 - d
-                f2, f1 = f1, self.f(x1)
-            else:  # x1 is new lower bound
-                x1, x2 = x2, x2 + d
-                f1, f2 = f2, self.f(x2)
-
-        return x1 if f1 > f2 else x2
+        return x if fx > fy else y
 
     def qnewton(self, x0=None, A=None, **kwargs):
 

compecon/tools.py
@@ -122,7 +122,7 @@ def jacobian(func, x, *args, **kwargs):
     #     F = lambda x: func(x, *args, **kwargs)
     F = lambda z: func(z, *args, **kwargs)
 
-    x = x.flatten()
+    x = np.asarray(x).flatten()
     dx = x.size
     f = F(x)
     df = f.size
@@ -150,9 +150,49 @@ def jacobian(func, x, *args, **kwargs):
     return fx.T
 
 
+def hess1(func, x, *args, **kwargs):
+
+    F = lambda z: func(z, *args, **kwargs)
+    x = np.asarray(x).flatten().astype(float)
+
+    k = x.size
+    fx = F(x)
+
+    '''Compute stepsize'''
+    tol = np.spacing(1) ** (1 / 4)
+    h = tol * np.maximum(np.abs(x), 1)
+    xh = x + h
+    h = xh - x
+    ee = np.diag(h)
+
+    '''Compute forward and backward steps'''
+    gplus = np.zeros_like(x)
+    gminus = np.zeros_like(x)
+    for i, ei in enumerate(ee):
+        gplus[i] = F(x + ei)
+        gminus[i] = F(x - ei)
+
+    H = np.outer(h, h)
+
+    for i in range(k):
+        for j in range(k):
+            if i==j:
+                H[i, i] = (gplus[i] + gminus[i] - 2*fx) / H[i, i]
+            else:
+                fxx = F(x + ee[i] - ee[j])
+                H[i, j] = (gplus[i] + gminus[j] - fx - fxx) / H[i, j]
+
+    return (H + H.T) / 2
+
+
+
+
+
+
 def hessian(func, x, *args, **kwargs):
 
     F = lambda z: func(z, *args, **kwargs)
+    x = np.asarray(x).flatten().astype(float)
     if x.ndim < 2:
         x = np.atleast_2d(x).T
 
@@ -198,7 +238,7 @@ def hessian(func, x, *args, **kwargs):
                 fxx[k, h] = (fpp + fmm - fpm - fmp) / (4 * deltaX[k] * deltaX[h])
 
     fxx = (fxx + fxx.swapaxes(0, 1)) / 2
-    return fxx
+    return fxx.squeeze()
 
 
 tic = lambda: time.time()